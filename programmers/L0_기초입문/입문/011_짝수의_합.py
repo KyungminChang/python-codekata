bf9b467cabe54a7844ec391f8dcb65e149038d58
kyungmin_CH <63341480+KyungminChang@users.noreply.github.com>
2026-01-19 09:39:11 +0900

    [프로그래머스] L0 - 짝수의 합 (#11)

--- programmers/L0_기초입문/입문/011_짝수의_합.py
+++ programmers/L0_기초입문/입문/011_짝수의_합.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120831
 # 알고리즘: 기초
 # 작성자: 장경민
-# 작성일: 2026. 01. 19. 09:38:49
+# 작성일: 2026. 01. 19. 09:39:10
 
 def solution(n):
     answer = 0
