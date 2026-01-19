4edad3e8f6c50da87a63391ba609fd62eb8d7b60
kyungmin_CH <63341480+KyungminChang@users.noreply.github.com>
2026-01-20 08:57:57 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,10 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 장경민
+# 작성일: 2026. 01. 20. 08:57:56
+
+def solution(n):
+    answer = int((n-1)/7) + 1
+    return answer
