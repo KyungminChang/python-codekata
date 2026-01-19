93762a5dcd7d8f144cfbcb21c88eb8c3bf8b89cd
kyungmin_CH <63341480+KyungminChang@users.noreply.github.com>
2026-01-19 09:52:41 +0900

    [프로그래머스] L0 - 뒤집힌 문자열 (#14)

---
+++ programmers/L0_기초입문/입문/014_뒤집힌_문자열.py
@@ -0,0 +1,12 @@
+# 뒤집힌 문자열
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120822
+# 알고리즘: 기초
+# 작성자: 장경민
+# 작성일: 2026. 01. 19. 09:52:40
+
+def solution(my_string):
+    answer = ''
+    answer = my_string[::-1]
+    print(answer)
+    return answer
