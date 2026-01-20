f0c29ba53b9fe06c163717f6530de59a9f942dc3
kyungmin_CH <63341480+KyungminChang@users.noreply.github.com>
2026-01-20 09:18:48 +0900

    [프로그래머스] L0 - 최댓값 만들기(1) (#18)

--- programmers/L0_기초입문/입문/018_최댓값_만들기_1_.py
+++ programmers/L0_기초입문/입문/018_최댓값_만들기_1_.py
@@ -3,17 +3,11 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120847
 # 알고리즘: 기초
 # 작성자: 장경민
-# 작성일: 2026. 01. 20. 09:17:10
+# 작성일: 2026. 01. 20. 09:18:48
 
 def solution(numbers):
-    max1 = 0
-    max2 = 0
-    
-    for n in numbers:
-        if n > max1:
-            max2 = max1 
-            max1 = n     
-        elif n > max2:
-            max2 = n     
-    answer  = max1 * max2
+    numbers.sort()
+    x= numbers[-1]
+    y= numbers[-2]
+    answer = x * y
     return answer
