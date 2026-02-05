40f37161a44407f70d64622ded7244002b62f379
kyungmin_CH <63341480+KyungminChang@users.noreply.github.com>
2026-02-05 14:47:37 +0900

    [프로그래머스] L0 - 배열 두 배 만들기 (#37)

---
+++ programmers/L0_기초입문/입문/037_배열_두_배_만들기.py
@@ -0,0 +1,13 @@
+# 배열 두 배 만들기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120809
+# 알고리즘: 기초
+# 작성자: 장경민
+# 작성일: 2026. 02. 05. 14:47:36
+
+def solution(numbers):
+    answer = []
+    for i in numbers : 
+        answer.append(i*2)
+            
+    return answer
